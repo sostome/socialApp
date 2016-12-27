892f4973f841838c67df24fcf393d868a5558ffe
sostome <baekjk@hanmail.net>
2016-12-27 20:33:27 +0900

    socialApp

--- gulp/css.js
+++ gulp/css.js
@@ -1,12 +1,13 @@
-var gulp   = require('gulp')
-var stylus = require('gulp-stylus')
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
 
-gulp.task('css', function () {
-  return gulp.src('css/app.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('assets'))
-})
+gulp.task('css', function(){
+  gulp.src('css/**/*.styl')
+		.pipe(stylus())		
+		.pipe(gulp.dest('assets'))
+});
 
-gulp.task('watch:css', ['css'], function () {
-  gulp.watch('css/**/*.styl', ['css'])
-})
+gulp.task('watch:css', function(){
+	gulp.watch('css/**/*.styl', ['css']);
+});
+
